4238f07bfe91f1d44b86bb4a5763264daaee810b
Roy <592151648@qq.com>
2016-05-16 02:03:33 +0800

    Service初步方法完成并测试

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -5,12 +5,17 @@ import org.seckill.dao.SuccessKilledDao;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStatEnum;
 import org.seckill.exception.RepeatKillException;
 import org.seckill.exception.SeckillCloseException;
 import org.seckill.exception.SeckillException;
 import org.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -19,12 +24,17 @@ import java.util.List;
 /**
  * Created by 59215_000 on 2016/5/15.
  */
+//@Component @Service @Dao @Controller
+@Service
 public class SeckillServiceImpl implements SeckillService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //注入service依赖
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     //md5盐值用于混淆
@@ -58,7 +68,51 @@ public class SeckillServiceImpl implements SeckillService {
         return md5;
     }
 
+    /**
+     * 使用注解控制方法的有点
+     * 1：开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2：保证事务方法的执行时间尽可能短，不要穿插其他的网络操作RPC/HTTP请求或者剥离到事务方法外
+     * 3：不是所有的方法都需要事务，只有一条修改操作，只读操作不需要事务控制
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     * @return
+     * @throws SeckillException
+     * @throws RepeatKillException
+     * @throws SeckillCloseException
+     */
+    @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
-        return null;
+
+        if (null == md5 || !md5.equals(this.getMD5(seckillId))) {
+            throw new SeckillException("seckill data rewrite");
+        }
+        //执行秒杀逻辑：减库存+记录购买
+        Date nowTime = new Date();
+        try {
+            int updateCount = seckillDao.reduceNumber(seckillId, nowTime);
+            if (updateCount <= 0) {
+                //没有记录更新，秒杀结束
+                throw new SeckillCloseException("seckill is close");
+            } else {
+                //记录购买行为
+                int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+                if (insertCount <= 0) {
+                    //重复秒杀
+                    throw new RepeatKillException("seckill repeated");
+                } else {
+                    //成功秒杀
+                    SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+                    return new SeckillExecution(seckillId, SeckillStatEnum.SUCCESS, successKilled);
+                }
+            }
+        } catch (SeckillCloseException e1) {
+            throw e1;
+        } catch (RepeatKillException e2) {
+            throw e2;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new SeckillException("seckill inner error:" + e.getMessage());
+        }
     }
 }
