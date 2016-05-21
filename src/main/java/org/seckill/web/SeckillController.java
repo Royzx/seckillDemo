59788ac612d87fea3c809c8bcd49aa0a149eb54c
Roy <592151648@qq.com>
2016-05-21 23:51:12 +0800

    springmvc 整合 controller设计

---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,102 @@
+package org.seckill.web;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.dto.SeckillResult;
+import org.seckill.entity.Seckill;
+import org.seckill.enums.SeckillStatEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by 59215_000 on 2016/5/21.
+ */
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public String list(Model model){
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list",list);
+
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail",method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId,Model model){
+        if (seckillId == null){
+            return "redirect:/seckill/list";
+        }
+
+        Seckill seckill = seckillService.getById(seckillId);
+
+        if (seckill == null){
+            return "redirect:/seckill/list";
+        }
+
+        model.addAttribute("seckill",seckill);
+
+        return "detail";
+    }
+
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true,exposer);
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            result = new SeckillResult<Exposer>(false,e.getMessage());
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+                    method = RequestMethod.POST,
+                    produces = {"application/json;charset=UTF-8"})
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @CookieValue(value = "killphone",required = false) Long phone){
+        if (phone == null){
+            return new SeckillResult<SeckillExecution>(false,"用户为注册");
+        }
+
+        try {
+            SeckillResult<SeckillExecution> result;
+            SeckillExecution execution = seckillService.executeSeckill(seckillId,phone,md5);
+            return new SeckillResult<SeckillExecution>(true,execution);
+        }catch (RepeatKillException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }catch (SeckillCloseException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }catch (SeckillException e){
+            logger.error(e.getMessage(),e);
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false,seckillExecution);
+        }
+    }
+}
+
